69b6650c61aac8d539257291db2b5301c85f5e5b
jaserino <jaserino1@gmail.com>
2023-11-03 18:15:23 -0400

    added type and hero section

---
+++ dist/script.js
@@ -0,0 +1,43 @@
+const words = ['Safe!', 'Educated!', 'Trained!'];
+
+let i = 0;
+let j = 0;
+let curr = '';
+let deleting = false;
+
+const typewriter = () => {
+  curr = words[i];
+
+  if (deleting) {
+    document.getElementById('typewriter').textContent = curr.substring(
+      0,
+      j - 1
+    );
+    j--;
+
+    if (j == 0) {
+      deleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+      setTimeout(typewriter, 1000);
+    } else {
+      setTimeout(typewriter, 200);
+    }
+  } else {
+    document.getElementById('typewriter').textContent = curr.substring(
+      0,
+      j + 1
+    );
+    j++;
+    if (j == curr.length) {
+      deleting = true;
+      setTimeout(typewriter, 1000);
+    } else {
+      setTimeout(typewriter, 200);
+    }
+  }
+};
+
+typewriter();
